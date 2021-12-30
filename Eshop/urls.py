64e11ae24e25aba0ee475ef8074d224abae9e92c
Dipak Rai <dipakrai7103@gmail.com>
2021-12-30 17:24:45 +0545

    fifth time

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('' , include('store.urls')),
     path('api-auth/', include('rest_framework.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 
     #REST FRAMEWORK URLS
